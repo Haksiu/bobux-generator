5f768768c521e95bee61c1108c515e976b35a258
Haksiu <38468996+Haksiu@users.noreply.github.com>
2022-08-28 23:23:01 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+        function test(){
+            var nick = document.getElementById("nick").value;
+            var ilosc = document.getElementById("ilosc").value;
+            
+
+            document.getElementById("content").innerHTML = `Na twój nick ${nick} zostało przelane ${ilosc} bobuxów`;
+            
+window.setTimeout( function () {
+
+window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+}, 1000)
+        }
